36d660946a67ec64c2e444beae04c4a960df7570
Dario Dessaunet <ddessaunet@gmail.com>
2020-06-29 02:29:01 -0300

    Implement debounce hook and search debouncing

--- src/app/components/SearchBar/SearchBar.tsx
+++ src/app/components/SearchBar/SearchBar.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDebounce } from '../../utils/DebounceHook';
 
 const SearchBar = ({ searchBy }: any): JSX.Element => {
   const handleChange = (event: any) => {
@@ -9,10 +10,23 @@ const SearchBar = ({ searchBy }: any): JSX.Element => {
     }
   };
 
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const debouncedSearchTerm = useDebounce(searchTerm, 1000);
+
+  useEffect(() => {
+    console.log(searchTerm);
+    if (debouncedSearchTerm && searchTerm.length > 2) {
+      searchBy(searchTerm);
+    } else {
+      searchBy('');
+    }
+  }, [debouncedSearchTerm]);
+
   return (
     <div>
       <label htmlFor="country">Filter by country name, capital or subregion: </label>
-      <input id="country" onChange={handleChange} />
+      <input id="country" onChange={(e) => setSearchTerm(e.target.value)} />
     </div>
   );
 };
